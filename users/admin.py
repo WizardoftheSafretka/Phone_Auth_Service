91e29a862559446dfa651db29c7f46e784615d22
Wizard <merihov2yandex.ru>
2026-07-15 22:17:48 +0300

    Создание модели пользователя и менеджеров

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('phone_number', 'invite_code', 'is_verified', 'is_active')
+    list_filter = ('is_verified', 'is_active', 'is_staff', 'is_superuser')
+    search_fields = ('phone_number', 'invite_code')
+    ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'password')}),
+        ('Инвайт-коды', {'fields': ('invite_code', 'activated_invite_code')}),
+        ('Статусы', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_verified')}),
+        ('Даты', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'password1', 'password2'),
+        }),
+    )
